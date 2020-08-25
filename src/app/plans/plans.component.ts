d6100486e42128147c119d90d1a0bc505f46545f
Matija Kokotovic <matijakokotovic@MacBook-Pro.local>
2020-08-25 16:10:56 +0200

    added filtering of tasks

--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -13,11 +13,11 @@ export class PlansComponent implements OnInit {
 
   currentDate: FormControl = new FormControl(new Date());
   planItems: Task[];
+  datedItems: Task[];
 
   constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.planItems = this.taskService.getAllTasks();
     if (this.route.snapshot.queryParams.date) {
       const paramDate = this.route.snapshot.queryParams.date;
       this.currentDate = new FormControl(new Date(this.route.snapshot.queryParams.date));
@@ -32,6 +32,8 @@ export class PlansComponent implements OnInit {
         queryParamsHandling: 'merge',
       });
     }
+    this.planItems = this.taskService.getAllTasks();
+    this.filterTasks(this.currentDate.value);
   }
 
   addProduct(): void {
@@ -40,12 +42,27 @@ export class PlansComponent implements OnInit {
 
   changeDate(): void {
     const date = this.currentDate.value;
+    this.filterTasks(date);
     this.router.navigate([], {
       relativeTo: this.route,
       // tslint:disable-next-line:radix
       queryParams: { date: (parseInt(String(date.getMonth())) + 1) + '-' + date.getDate() + '-' + date.getFullYear()},
       queryParamsHandling: 'merge',
     });
+  }
+
+  filterTasks(date): void {
+    this.datedItems = [];
+    // tslint:disable-next-line:radix
+    const compareDate = (parseInt(String(date.getMonth())) + 1) + '-' + date.getDate() + '-' + date.getFullYear();
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.planItems.length; i++) {
+      // tslint:disable-next-line:max-line-length radix
+       const setDate = (parseInt(String(this.planItems[i].date.getMonth())) + 1) + '-' + this.planItems[i].date.getDate() + '-' + this.planItems[i].date.getFullYear();
+       if(compareDate === setDate) {
+         this.datedItems.push(this.planItems[i]);
+       }
+    }
   }
 
 }
